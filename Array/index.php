3b98442549a0bade49d1376334b3b1abc0478e53
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-20 02:46:24 +0530

    Array

---
+++ Array/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Array
+
+   $foods = array("apple", "orange", "banana", "coconut");
+
+  //  echo $foods[0] . "<br>";
+  //  echo $foods[1] . "<br>";
+  //  echo $foods[2] . "<br>";
+  //  echo $foods[3] . "<br>";
+  //  echo $foods[4] . "<br>";
+
+  // $foods[0] = "pineapple"; // Change the value of an item in the array
+  //array_push($foods, "pineapple"); // Add an item to the end of the array
+  //array_pop($foods); // Remove the last item from the array
+  //array_shift($foods); // Remove the first item from the array
+  //$reversed_foods = array_reverse($foods); // Reverse the order of the array
+  //echo count($foods); // Count the number of items in the array
+
+  // For each loop = to loop through each item in an array
+  // foreach($foods as $food){
+  //   echo $food . "<br>";
+  // }
+
+
+?>
