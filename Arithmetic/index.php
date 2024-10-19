51d014dab2e174d164f17f9200e426af7ad49dba
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-19 20:53:14 +0530

    Arithmetic

---
+++ Arithmetic/index.php
@@ -0,0 +1,56 @@
+<?php
+
+  // // Arithmetic Operators
+
+  // $x = 10;
+  // $y = 5;
+  // $z = null;
+
+  // // Addition
+  // $z = $x + $y;
+  // echo "Addition: {$z} <br>";
+
+  // // Subtraction
+  // $z = $x - $y;
+  // echo "Subtraction: {$z} <br>";
+
+  // // Multiplication
+  // $z = $x * $y;
+  // echo "Multiplication: {$z} <br>";
+
+  // // Division
+  // $z = $x / $y;
+  // echo "Division: {$z} <br>";
+
+  // // Modulus
+  // $z = $x % $y;
+  // echo "Modulus: {$z} <br>";
+
+  // // Exponentiation
+  // $z = $x ** $y;
+  // echo "Exponentiation: {$z} <br>";
+
+  // // Increment/Decrement Operators
+
+  // $counter = 0;
+
+  // // Increment
+  // $counter++;
+  // echo "Counter: {$counter} <br>";
+
+  // // Decrement
+  // $counter--;
+  // echo "Counter: {$counter} <br>";
+
+  // Operator Precedence
+
+  $total = 1 + 2 - 3 * 4 / 5 ** 6;
+  // $total = 1 + 2 - 3 * 4 / 15625;
+  // $total = 1 + 2 - 12 / 15625;
+  // $total = 1 + 2 - 0.000768;
+  // $total = 3 - 0.000768;
+  // $total = 2.999232;
+  echo "Total: {$total} <br>"; // 2.999232
+
+  
+?>
