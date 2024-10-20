945914cb48150d9328fa23a15fd672f262a0923e
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-20 12:05:06 +0530

    String Functions

---
+++ String-Functions/index.php
@@ -0,0 +1,28 @@
+<?php
+  $username = "Nipuna Lakruwan";
+  $phone = "0712345678";
+
+  //$username = strtolower($username); // nipuna lakruwan
+  //$username = strtoupper($username); // NIPUNA LAKRUWAN
+  //$username = trim($username); // Nipuna Lakruwan
+  // $username = str_pad($username, 20, "0"); // Nipuna Lakruwan000000
+  //$username = str_replace("Nipuna", "John", $username); // John Lakruwan
+  //$username = str_repeat($username, 2); // Nipuna LakruwanNipuna Lakruwan
+  //$username = str_shuffle($username); // nLpauLaninuak
+  //$username = str_word_count($username); // 2
+  //$username = strrev($username); // nawurkaL anupiN
+  //$username = strlen($username); // 14
+  //$username = strpos($username, " "); // 6
+  //$username = substr($username, 0, 6); // Nipuna
+  //$username = ucfirst($username); // Nipuna
+  //$username = ucwords($username); // Nipuna Lakruwan
+  //$username = explode(" ", $username); // Array
+  //$username = implode("-", $username); // Nipuna Lakruwan
+
+  // foreach($username as $name){
+  //   echo $name . "<br>";
+  // }
+
+
+  echo $username;
+?>
